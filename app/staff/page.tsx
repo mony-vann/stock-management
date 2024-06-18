1b9e5becaaf9020f6b99734e8acb8706aa30bf52
zenyyang <2022147men@aupp.edu.kh>
2024-06-18 11:27:04 +0700

    change fetching

--- app/staff/page.tsx
+++ app/staff/page.tsx
@@ -4,27 +4,29 @@ import { db } from "@/lib/db";
 import StaffClient from "./_components/client";
 import { StaffColumn } from "./_components/column";
 
-const StaffPage = async () => {
+// Fetching data on the server-side
+const fetchStaffData = async () => {
   const data = await db.employee.findMany({
     include: {
       shifts: true,
     },
   });
 
-  const formattedData: StaffColumn[] = data.map((item) => {
-    return {
-      id: item.id,
-      name: item.name,
-      contact_info: item.contact_info,
-      role: item.role,
-      shifts: item.shifts,
-    };
-  });
+  return data.map((item) => ({
+    id: item.id,
+    name: item.name,
+    contact_info: item.contact_info,
+    role: item.role,
+    shifts: item.shifts,
+  }));
+};
 
+const StaffPage = async () => {
+  const data = await fetchStaffData();
   return (
     <div className="flex min-h-screen w-full flex-col bg-muted/40 pt-5">
       <div className="flex flex-col sm:gap-4 sm:py-4 sm:pl-14">
-        <StaffClient data={formattedData} />
+        <StaffClient data={data} />
       </div>
     </div>
   );
