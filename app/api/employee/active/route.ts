184ec4f3a147e19b985b6684d1df25064a272e9f
zenyyang <2022147men@aupp.edu.kh>
2024-06-27 19:41:49 +0700

    test database

--- app/api/employee/active/route.ts
+++ app/api/employee/active/route.ts
@@ -2,40 +2,41 @@ import { NextResponse } from 'next/server';
 import { db } from '@/lib/db';
 
 export async function GET() {
-    const allLogs = await db.attendance.findMany({
-        where: {
-            timestamp: {
-                gte: new Date(new Date().setHours(0, 0, 0, 0)), // Today's date
-            },
-        },
-        orderBy: {
-            timestamp: 'desc',
-        },
-        select: {
-            employee: {
-                select: {
-                    name: true,
-                    role: true,
-                },
-            },
-            timestamp: true,
-            type: true,
-        },
-    });
+    // const allLogs = await db.attendance.findMany({
+    //     where: {
+    //         timestamp: {
+    //             gte: new Date(new Date().setHours(0, 0, 0, 0)), // Today's date
+    //         },
+    //     },
+    //     orderBy: {
+    //         timestamp: 'desc',
+    //     },
+    //     select: {
+    //         employee: {
+    //             select: {
+    //                 name: true,
+    //                 role: true,
+    //             },
+    //         },
+    //         timestamp: true,
+    //         type: true,
+    //     },
+    // });
 
-    const groupedLogs = allLogs.reduce((acc: any, log) => {
-        const name = log.employee.name;
-        if (!acc[name]) {
-            acc[name] = [];
-        }
-        acc[name].push(log);
-        return acc;
-    }, {});
+    // const groupedLogs = allLogs.reduce((acc: any, log) => {
+    //     const name = log.employee.name;
+    //     if (!acc[name]) {
+    //         acc[name] = [];
+    //     }
+    //     acc[name].push(log);
+    //     return acc;
+    // }, {});
 
-    const activeStaffs = Object.values(groupedLogs)
-        .map((logs: any) => logs.reduce((a:any, b:any) => (a.timestamp > b.timestamp ? a : b)))
-        .filter((log) => log.type === 'check-in');
+    // const activeStaffs = Object.values(groupedLogs)
+    //     .map((logs: any) => logs.reduce((a:any, b:any) => (a.timestamp > b.timestamp ? a : b)))
+    //     .filter((log) => log.type === 'check-in');
 
 
-    return NextResponse.json(activeStaffs);
+    // return NextResponse.json(activeStaffs);
+    return NextResponse.json('activeStaffs');
 }
