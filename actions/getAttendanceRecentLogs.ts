fa69ef2e46034e7b541edb922ec0f58f87f0982f
zenyyang <2022147men@aupp.edu.kh>
2024-06-27 19:45:50 +0700

    reset database

--- actions/getAttendanceRecentLogs.ts
+++ actions/getAttendanceRecentLogs.ts
@@ -46,18 +46,18 @@ export const getActiveStaffs = async () => {
         timestamp: true,
         type: true,
     },
-})
+  })
 
-const groupedLogs = allLogs.reduce((acc: any, log) => {
+  const groupedLogs = allLogs.reduce((acc: any, log) => {
     const name = log.employee.name;
     if (!acc[name]) {
         acc[name] = [];
     }
     acc[name].push(log);
     return acc;
-}, {});
+  }, {});
 
-const activeStaffs = Object.values(groupedLogs)
+  const activeStaffs = Object.values(groupedLogs)
     .map((logs: any) => logs.reduce((a:any, b:any) => (a.timestamp > b.timestamp ? a : b)))
     .filter((log) => log.type === 'check-in');
 
